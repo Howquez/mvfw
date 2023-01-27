471d5f73567a5c846fc933695243d664180aaadd
Hauke Roggenkamp <haukeroggenkamp@eduroamverw-130-82-31-179.unisg.ch>
2023-01-27 16:29:19 +0100

    add column design to implement ads and tweet button

--- otree/feed/static/js/like_button.js
+++ otree/feed/static/js/like_button.js
@@ -3,7 +3,9 @@ console.log("like button ready!");
 function like() {
     var attention_check = document.getElementById("like_button")
     attention_check.className="bi bi-heart-fill text-danger"
-    attention_check.innerHTML="&nbsp;5&emsp;"
+    n_likes = Number(attention_check.innerHTML.match(/\d+/)[0])
+    attention_check.innerHTML="&nbsp;" + (n_likes + 1) + "&nbsp;"
+    attention_check.removeAttribute("onclick");
 
     var redirect_button = document.getElementById("submitButton")
     var submit_button = document.createElement("button")
@@ -11,4 +13,6 @@ function like() {
     submit_button.className = "btn btn-outline-dark btn-sm m-2"
     submit_button.innerHTML = "Submit"
     redirect_button.parentNode.replaceChild(submit_button, redirect_button);
+
+    attention_check.removeAttribute("onclick");
 }
